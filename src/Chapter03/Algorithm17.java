f924ad4554fbb3dd0312afa02d62b30d0fe5bf58
Wobum <17720819850@163.com>
2019-01-21 17:58:39 +0800

    字符串模拟数字的加法

---
+++ src/Chapter03/Algorithm17.java
@@ -0,0 +1,73 @@
+package Chapter03;
+
+/**
+ * @Author : Wobum
+ * @Date : 2019/1/20 21:05
+ * @Software : IntelliJ IDEA
+ * @Description: 输入数字 n,按顺序打印出从 1 到最大的 n 位十进制
+ **/
+public class Algorithm17 {
+    public static void printToMaxOfDigits(int n) {
+        if (n <= 0)
+            return;
+        char[] chars = new char[n];
+        for (int i = 0; i < n; i++) {
+            chars[i] = '0';
+        }
+        while (!increment(chars)) {
+            printCharArr(chars);
+        }
+    }
+
+    public static boolean increment(char[] chars){
+        // 溢出标志位
+        boolean isOverFlow = false;
+        // 进位值
+        int carry = 0;
+        int index = chars.length - 1;
+
+        while (index >= 0){
+            // num 表示当前位置的字符与字符 '0' 的偏移量
+            int num = chars[index] - '0' + carry;
+            // 如果当前字符位置是个位，直接 ++
+            if (index == chars.length - 1){
+                num++;
+            }
+            // 如果大于 10，产生进位，当前位置置为 0，进入循环
+            if (num >= 10){
+                if (index == 0){
+                    isOverFlow = true;
+                }else {
+                    num = num % 10;
+                    carry = 1;
+                    chars[index] = '0';
+                    index--;
+                }
+            }else {
+                // 小于 10，直接设置，退出循环
+                chars[index] = (char)(num + '0');
+                break;
+            }
+        }
+        return isOverFlow;
+    }
+
+    // 打印字符数组
+    public static void printCharArr(char[] chars) {
+        boolean begin = false;
+        for (int i = 0; i < chars.length; i++) {
+            if (!begin && chars[i] != '0')
+                begin = true;
+            if (begin) {
+                System.out.print(chars[i]);
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        /*char[] arr = {'0','1','0'};
+        printCharArr(arr);*/
+        printToMaxOfDigits(2);
+    }
+}
