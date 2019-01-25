ef99636b189abef2641dbc0a7619be017b17365e
Wobum <17720819850@163.com>
2019-01-25 17:21:26 +0800

    从 1 开始 n 位整数

--- src/Chapter03/Algorithm17.java
+++ src/Chapter03/Algorithm17.java
@@ -7,6 +7,7 @@ package Chapter03;
  * @Description: 输入数字 n,按顺序打印出从 1 到最大的 n 位十进制
  **/
 public class Algorithm17 {
+    // 字符数组模拟数字加法实现
     public static void printToMaxOfDigits(int n) {
         if (n <= 0)
             return;
@@ -19,33 +20,33 @@ public class Algorithm17 {
         }
     }
 
-    public static boolean increment(char[] chars){
+    public static boolean increment(char[] chars) {
         // 溢出标志位
         boolean isOverFlow = false;
         // 进位值
         int carry = 0;
         int index = chars.length - 1;
 
-        while (index >= 0){
+        while (index >= 0) {
             // num 表示当前位置的字符与字符 '0' 的偏移量
             int num = chars[index] - '0' + carry;
             // 如果当前字符位置是个位，直接 ++
-            if (index == chars.length - 1){
+            if (index == chars.length - 1) {
                 num++;
             }
             // 如果大于 10，产生进位，当前位置置为 0，进入循环
-            if (num >= 10){
-                if (index == 0){
+            if (num >= 10) {
+                if (index == 0) {
                     isOverFlow = true;
-                }else {
+                } else {
                     num = num % 10;
                     carry = 1;
                     chars[index] = '0';
                     index--;
                 }
-            }else {
+            } else {
                 // 小于 10，直接设置，退出循环
-                chars[index] = (char)(num + '0');
+                chars[index] = (char) (num + '0');
                 break;
             }
         }
@@ -65,9 +66,31 @@ public class Algorithm17 {
         System.out.println();
     }
 
+    // 全排列进行打印，递归实现
+    public static void printToMaxOfDigits1(int n) {
+        if (n <= 0)
+            return;
+        char[] chars = new char[n];
+        for (int i = 0; i < 10; i++) {
+            chars[0] = (char) (i + '0');
+            recursively(chars, n, 0);
+        }
+    }
+
+    public static void recursively(char[] chars, int length, int index) {
+        if (index == length - 1) {
+            printCharArr(chars);
+            return;
+        }
+        for (int i = 0; i < 10; i++) {
+            chars[index + 1] = (char) (i + '0');
+            recursively(chars, length, index + 1);
+        }
+    }
+
     public static void main(String[] args) {
         /*char[] arr = {'0','1','0'};
         printCharArr(arr);*/
-        printToMaxOfDigits(2);
+        printToMaxOfDigits1(2);
     }
 }
